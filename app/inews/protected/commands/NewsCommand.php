cb861685a9265ef418fdcf13796609e099d7e937
trungbq06@gmail.com <trungbq06@gmail.com@6458101b-43cc-ddaa-8d4a-56acf053dbe4>
2011-12-08 14:58:40 +0000

    Add command

---
+++ app/inews/protected/commands/NewsCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+class NewsCommand extends CConsoleCommand {
+
+	public function actionIndex($args = array()) {
+		$crawler = new Crawler();
+		$crawler->getVnexpress();
+		$crawler->getExchange();
+		$crawler->getGold();
+		$crawler->getOil();
+		$crawler->getWeather();
+		$crawler->getLottery();
+		$crawler->getDantri();
+		
+		$crawler->getVnexpressVideo();
+		$crawler->getTvCalendar();
+		$crawler->getLottery();
+		$crawler->getCinemaSchedule();
+		$crawler->getTvCalendar();
+	}
+
+}
